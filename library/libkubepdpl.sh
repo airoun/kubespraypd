2eb9d8c49a6b4019ae6ca629a7fee36e6f88f958
baiyitong <potato210402@aliyun.com>
2020-07-30 11:10:21 +0800

    fix some bugs

--- library/libkubepdpl.sh
+++ library/libkubepdpl.sh
@@ -100,7 +100,7 @@ print_kubespray_environment() {
 docker_rh_repo_base_url: \"${online_docker_rh_repo_url}\"
 docker_rh_repo_gpgkey: \"${online_docker_rh_repo_gpgkey}\"
 
-# Kubernetes image repo
+# Image repo
 kube_image_repo: \"${online_kube_image_repo}\"
 docker_image_repo: \"${online_docker_image_repo}\"
 quay_image_repo: \"${online_quay_image_repo}\"
@@ -109,7 +109,6 @@ quay_image_repo: \"${online_quay_image_repo}\"
 kubeadm_download_url: \"http://${online_kubedpdl_http_repo}/kubernetes-release/release/{{ kube_version }}/bin/linux/{{ image_arch }}/kubeadm\"
 kubelet_download_url: \"http://${online_kubedpdl_http_repo}/kubernetes-release/release/{{ kube_version }}/bin/linux/{{ image_arch }}/kubelet\"
 kubectl_download_url: \"http://${online_kubedpdl_http_repo}/kubernetes-release/release/{{ kube_version }}/bin/linux/{{ image_arch }}/kubectl\"
-
 etcd_download_url: \"http://${online_kubedpdl_http_repo}/coreos/etcd/releases/download/{{ etcd_version }}/etcd-{{ etcd_version }}-linux-{{ image_arch }}.tar.gz\"
 cni_download_url: \"http://${online_kubedpdl_http_repo}/containernetworking/plugins/releases/download/{{ cni_version }}/cni-plugins-linux-{{ image_arch }}-{{ cni_version }}.tgz\"
 calicoctl_download_url: \"http://${online_kubedpdl_http_repo}/projectcalico/calicoctl/releases/download/{{ calico_ctl_version }}/calicoctl-linux-{{ image_arch }}\"
--- scripts/update_images.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/evn bash
-# Update images, pull new images from gcr.io, quay.io, docker.io and push them to registry.cn-hangzhou.aliyuncs.com
