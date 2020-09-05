26441c5498e25081b20d116d9577cd1b6c9b2bb1
baiyitong <potato210402@aliyun.com>
2020-09-05 16:55:46 +0800

    fix some bugs

--- library/libkubepd.sh
+++ library/libkubepd.sh
@@ -10,7 +10,7 @@ kubespray_predeploy_download() {
   can_i_connect_to_internet
 
   dl_kubespray_code "${project_base_dir}/files"
-  dl_kubespray_files "${project_base_dir}/files/"
+  dl_kubespray_files "${project_base_dir}/files"
 
   if [[ "${download_centos_iso_enable}X" == "trueX" ]];then
     dl_centos_isos "${project_base_dir}/isos"
